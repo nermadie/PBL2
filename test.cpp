59633edde3bcc31efd2571690d0a72ac18aeea71
nermadie <congtudapchai@gmail.com>
2021-12-15 01:50:31 +0700

    Update main

--- test.cpp
+++ test.cpp
@@ -1,33 +1,33 @@
-#include "QuanLy/QLNV.cpp"
-#include "Object/Film.cpp"
-bool GD(int a, int b)
-{
-    return a < b;
-}
-bool TD(int a, int b)
-{
-    return a > b;
-}
-int main()
-{
-    try
-    {
-        // QLNV temp;
-        // temp.Show();
-        // temp.Sort();
-        // temp.Show();
-        Film F(1, "song gio", "nhat minh", "Hanh dong", 107);
-        F.ShowPhim();
-    }
-    catch (const char *msg)
-    {
-        cout << endl
-             << msg;
-    }
-    catch (string msg)
-    {
-        cout << endl
-             << msg;
-    }
-    return 0;
-}
+// #include "QuanLy/QLNV.cpp"
+// #include "Object/Film.cpp"
+// bool GD(int a, int b)
+// {
+//     return a < b;
+// }
+// bool TD(int a, int b)
+// {
+//     return a > b;
+// }
+// int main()
+// {
+//     try
+//     {
+//         // QLNV temp;
+//         // temp.Show();
+//         // temp.Sort();
+//         // temp.Show();
+//         Film F(1, "song gio", "nhat minh", "Hanh dong", 107);
+//         F.ShowPhim();
+//     }
+//     catch (const char *msg)
+//     {
+//         cout << endl
+//              << msg;
+//     }
+//     catch (string msg)
+//     {
+//         cout << endl
+//              << msg;
+//     }
+//     return 0;
+// }
